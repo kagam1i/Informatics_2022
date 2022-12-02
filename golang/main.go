9ea8db8196405ab6cae6ba357f6825a2d9de1a3e
kagam1i <evb1404@gmail.com>
2022-12-02 23:08:05 +0300

    Решение задач на codewars

--- golang/main.go
+++ golang/main.go
@@ -8,10 +8,21 @@ import (
 func main() {
 	const a float64 = 2.0
 	const b float64 = 0.95
+	var s int
+	fmt.Scan(&s)
+	var k int
+	fmt.Scan(&k)
+	var n, m int
+	fmt.Scan(&n)
+	fmt.Scan(&m)
 	fmt.Println("Задание А")
 	fmt.Println(TaskA(a, b))
 	fmt.Println("Задание Б")
 	fmt.Println(TaskB(a, b))
+	fmt.Println(EvenOrOdd(s))
+	fmt.Println(CountingSheep())
+	fmt.Println(CountTheMonkeys(k))
+	fmt.Println(SchoolPaperwork(n, m))
 }
 
 func TaskA(a, b float64) []float64 {
@@ -35,4 +46,43 @@ func TaskB(a, b float64) []float64 {
 func formula(x, a, b float64) float64 {
 	var y float64 = (1 + math.Pow(math.Log10(a/b), 2.0)) / (b - math.Pow(2.718, x/a))
 	return y
+}
+
+func EvenOrOdd(s int) string {
+	var result string
+	if s%2 == 0 {
+		result = "Even"
+	} else if s%2 != 0 {
+		result = "Odd"
+	}
+	return result
+}
+
+func CountingSheep() int {
+	var sheeps = []string{"true", "true", "true", "false", "true", "false", "true", "true"}
+	var count int = 0
+	for i := 0; i < len(sheeps); i++ {
+		if sheeps[i] == "true" {
+			count++
+		}
+	}
+	return count
+}
+
+func CountTheMonkeys(k int) []int {
+	var numbers []int
+	for i := 1; i <= k; i++ {
+		numbers = append(numbers, i)
+	}
+	return numbers
+}
+
+func SchoolPaperwork(n, m int) int {
+	var paper int
+	if n > 0 && m > 0 {
+		paper = n * m
+	} else {
+		paper = 0
+	}
+	return paper
 }
